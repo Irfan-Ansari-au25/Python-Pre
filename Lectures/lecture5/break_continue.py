8c48f152dc01d64734a034909fb586dc0f38218a
phoreka <irfan398649@gmail.com>
2021-06-17 17:23:58 +0530

    While loop, continue and break

---
+++ Lectures/lecture5/break_continue.py
@@ -0,0 +1,16 @@
+"""
+break terminates the control loop 
+continue skips code after that 
+
+"""
+
+
+
+
+
+for i in range(5):
+    if (i <3):
+        continue
+    if (i == 4):
+        break
+    print(i)
---
+++ Lectures/lecture5/sumofcubes.py
@@ -0,0 +1,14 @@
+sum = 0
+
+# for i in range(1,5):
+#     print( i, "iteration ", sum)
+#     sum += i**3
+# print(sum)
+
+
+count = 1
+
+while(count<5):
+    sum += count ** 3
+    count += 1
+print("sum of the cubes", sum)
