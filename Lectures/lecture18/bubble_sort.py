50bb66d46eb0f1d3c9605b9603bb9cef4776875d
phoreka <irfan398649@gmail.com>
2021-07-23 17:10:37 +0530

    beer song

--- Lectures/lecture18/bubble_sort.py
+++ Lectures/lecture18/bubble_sort.py
@@ -6,7 +6,7 @@ for i in range(len(A) - 1) :
     
 
     for j in range(0, len(A) - i - 1):
-        print(j, "loop j")
+        print(j, "loop j",A[j],A[j+1])
         if A[j] > A[j + 1]:
             A[j], A[j + 1] = A[j + 1], A[j]
         
