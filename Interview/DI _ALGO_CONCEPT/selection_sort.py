36cb535823dbf16553b8a52a38d98576463049ac
phorekka <irfan398649@gmail.com>
2021-08-21 08:02:20 +0530

    leetcode and matrix problem

---
+++ Interview/DI _ALGO_CONCEPT/selection_sort.py
@@ -0,0 +1,31 @@
+
+
+
+A = [7,3,8,9,2,11]
+# find smallest number
+#  exchange that number with A[1]
+# continue for n-1 elements 
+
+l = len(A)
+
+# for i in range(l-1):
+#     smallest = A[i]
+#     idx = 0
+#     for j in range(1,l):
+#         if A[j] < smallest:
+#             smallest = A[j]
+#             idx = j
+#             print(smallest,i,j)
+#     # A[i],smallest = smallest,A[i]
+# print(A)
+# for j in range(5):
+#     s = A[j]
+#     for i in range(j+1,l):
+#         if A[i] < s:
+#             s = A[i]
+#     A[j], s = s, A[j]
+
+S = 'MY NAME, IS JAZIB.'
+print(S.split(','))
+
+
---
+++ Interview/RandomQuestions/matrix_prob.py
@@ -0,0 +1,46 @@
+
+"""
+# A = [
+#         ['#', '#', '#'],
+#         ['#', '', '#'],
+#         ['#', '#', '#'],
+# ]
+
+
+for 3 X 3 gap = 1         idx =[ (1,)]
+for 4 X 4 gap = 2**2      idx = [(1,1),(1,2),(2,1),(2,2)]
+for 5 X 5 gap = 3**3      idx = [((1,1),(1,2),(1,3),(2,1),(2,2),(2,3),(3,1),(3,2),(3,3))]
+"""
+ip= int(input('Enter row or column of a square matrix greater than 3 : '))
+A = [[0 for i in range(ip)] for j in range(ip)]
+# print(A)
+rows = len(A)
+cols = len(A[0])
+list_of_non_border_ele = []
+
+def findIdxOfGap(rows,cols):
+    for row in range(1, rows-1):
+        for col in range(1, cols-1):
+            # print(row,col)
+            list_of_non_border_ele.append((row,col))
+
+
+findIdxOfGap(rows,cols)
+
+# print(list_of_non_border_ele)
+
+def printBorderElements(rows,cols):
+    for row in range(rows):
+        for col in range(cols):
+            if (row,col) in list_of_non_border_ele:
+                A[row][col] = ' '
+                print(A[row][col], end=' ')
+            else:
+                A[row][col] = '#'
+                print(A[row][col], end=' ')
+        print()
+printBorderElements(rows,cols)
+# print(A)
+
+
+
